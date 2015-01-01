16a4fad40dfb6fa2c1c84a2ed3fef6312234fbd9
Vikramnadathur <nadathurvikram@gmail.com>
2015-01-01 13:27:11 -0500

    second version
    added import statement

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,7 @@
-
+/*
+ * This is a calculator exercise.
+ */
+import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Now to do some operations.");
