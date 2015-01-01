778cade206d225bcf71214c2c939597518c2f230
Vikramnadathur <nadathurvikram@gmail.com>
2015-01-01 18:14:21 -0500

    fix calc
    Added all functions and changed double to int. But there is a
    question...

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,16 +3,30 @@
  */
 
 public class Calculator {
+	 static double num_7;
+	 /*
+	  * Question: When creating a function do the types of the 
+	  * parameter have to be defined right their or can they be
+	  * defined at the top?
+	  */
 	public static void main(String[] args) {
 		System.out.println("Now to do some operations.");
 		
 	}
-	public static void add(int num_1,int num_2){
-		int num_3=num_1+num_2;
+	public static void add(double num_1,double num_2){
+		double num_3=num_1+num_2;
 		System.out.println(num_3);
 	}
-	public static void sub(int num_4,int num_5){
-		int num_6 = num_4 - num_5;
+	public static void sub(double num_4,double num_5){
+		double num_6 = num_4 - num_5;
 		System.out.println(num_6);
+	}
+	public static void divide(double num_7, double num_8) {
+		double num_9= num_7/num_8;
+		System.out.println(num_9);
+	}
+	public static void multi(double num_10, double num_11) {
+		double num_12= num_10*num_11;
+		System.out.println(num_12);
 	}
 }	
