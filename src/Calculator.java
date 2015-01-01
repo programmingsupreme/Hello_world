067db5c583f3b4b2b6c78d10aff6d7b789fe770c
Vikramnadathur <nadathurvikram@gmail.com>
2015-01-01 17:51:28 -0500

    Calc edit
    added add and subtract methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,18 @@
 /*
  * This is a calculator exercise.
  */
-import java.util.Scanner;
+
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Now to do some operations.");
 		
+	}
+	public static void add(int num_1,int num_2){
+		int num_3=num_1+num_2;
+		System.out.println(num_3);
+	}
+	public static void sub(int num_4,int num_5){
+		int num_6 = num_4 - num_5;
+		System.out.println(num_6);
 	}
 }	
